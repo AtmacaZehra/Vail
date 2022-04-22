52140e5e571ae0911055850f6008a71578346f55
zelemele2013 <zeraatmaca@gmail.com>
2022-04-22 16:07:09 -0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,5 +32,21 @@ public class Main {
 
     }
 
+
+    static int[] shiftArray2(int[] arr, int times){
+        int len= arr.length;
+        if (len<=1) return arr;
+        int shift = times%len;
+        if (shift==0) return arr;
+        int[] result=new int[len];
+
+        for (int i = 0; i < result.length; i++, shift++) {
+            if(shift<len)result[i] = arr[shift];
+            else result[i]=arr[shift-len];
+        }
+
+        return result;
+    }
+
 }
 
